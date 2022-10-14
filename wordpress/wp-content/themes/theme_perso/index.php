55467d3ed5f9027aa3e073ca8c594704427632b0
Rizo <g.rizzoli@hotmail.ca>
2022-10-14 17:49:20 -0400

    hiérarchie du nouveau theme

---
+++ wordpress/wp-content/themes/theme_perso/index.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package igc31w
+ */
+
+// get_header();
+ wp_head();
+
+
+?>
+<h1>Index.php</h1>
+	<ul>
+        <li>Bonjour</li>
+        <li>Mon nom</li>
+        <li>est</li>
+        <li>Bonjour</li>
+        <li>Bonjour</li>
+    </ul>
+	</main><!-- #main -->
+<?php
+get_footer();
+
