6a8dbd1346180f4621f140efdbd6fb6c9cd9f70f
Rizo <g.rizzoli@hotmail.ca>
2022-12-12 23:59:27 -0500

    changement de la chaine de caractere

--- template-parts/accueil-cours.php
+++ template-parts/accueil-cours.php
@@ -9,7 +9,7 @@ $le_permalien = "<a href='" . get_the_permalink() . "'>Suite</a>";
 <article class="grille__article">
     <?php the_post_thumbnail ( 'medium' ); ?>
     <h6><?=  $titre;  ?></h6>
-    <p><?= wp_trim_words(get_the_excerpt(), 10,$le_permalien) ; ?></p>
-    <p>Type de cours:<?php the_field("type_de_cours") ?></p>
+    <p><?= wp_trim_words(get_the_excerpt(), 10);?> <?=$le_permalien; ?></p>
+    <p>Type de cours: <?php the_field("type_de_cours") ?></p>
     <p>TIM - Collège de Maisonneuve</p>
 </article>
